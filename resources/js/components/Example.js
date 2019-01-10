e1a79c0f0f11b6925a34b5eb15f3bea151ff5929
damiadek <damiadek2009@gmail.com>
2019-01-10 14:36:56 +0100

    added reactstrap

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Button, Row, Col } from "reactstrap";
 
 export default class Example extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
-
-                            <div className="card-body">
-                                I'm an example component!
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Row>
+                <Col sm="12" md="6" lg="4">
+                    <Button color="primary">Hello stuff</Button>
+                </Col>
+            </Row>
         );
     }
 }
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+if (document.getElementById("example")) {
+    ReactDOM.render(<Example />, document.getElementById("example"));
 }
